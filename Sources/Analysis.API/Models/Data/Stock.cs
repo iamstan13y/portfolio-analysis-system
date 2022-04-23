2348137dcec260e98213ccfdc1740b6b606cba5d
iamstan13y <iamstan13y@gmail.com>
2022-04-23 16:38:09 +0200

    Stock Repository Interface & Model.

---
+++ Sources/Analysis.API/Models/Data/Stock.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Analysis.API.Models.Data
+{
+    public class Stock
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? CompanyName { get; set; }
+        public double UnitPrice { get; set; }
+        public double MinPrice { get; set; }
+        public double MaxPrice { get; set; }
+    }
+}
---
+++ Sources/Analysis.API/Models/Repository/IStockRepository.cs
@@ -0,0 +1,12 @@
+﻿using Analysis.API.Models.Data;
+using ModelLibrary;
+
+namespace Analysis.API.Models.Repository
+{
+    public interface IStockRepository
+    {
+        Task<Result<IEnumerable<Stock>>> GetAllAsync();
+        Task<Result<Stock>> AddAsync(Stock stock);
+        Task<Result<Stock>> UpdateAsync(Stock stock);
+    }
+}
