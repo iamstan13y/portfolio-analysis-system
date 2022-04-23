7cc7e75f7a707bbd4d77e56bb138e6b0acca6b84
iamstan13y <iamstan13y@gmail.com>
2022-04-23 17:15:23 +0200

    Get All Stock Impl.

---
+++ Sources/Analysis.API/Controllers/StocksController.cs
@@ -0,0 +1,21 @@
+﻿using Analysis.API.Models.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Analysis.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class StocksController : ControllerBase
+    {
+        private readonly IStockRepository _stockRepository;
+
+        public StocksController(IStockRepository stockRepository)
+        {
+            _stockRepository = stockRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(await _stockRepository.GetAllAsync());
+    }
+}
--- Sources/Analysis.API/Models/Repository/StockRepository.cs
+++ Sources/Analysis.API/Models/Repository/StockRepository.cs
@@ -1,18 +1,27 @@
 ﻿using Analysis.API.Models.Data;
+using Microsoft.EntityFrameworkCore;
 using ModelLibrary;
 
 namespace Analysis.API.Models.Repository
 {
     public class StockRepository : IStockRepository
     {
+        private readonly AppDbContext _context;
+
+        public StockRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public Task<Result<Stock>> AddAsync(Stock stock)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Result<IEnumerable<Stock>>> GetAllAsync()
+        public async Task<Result<IEnumerable<Stock>>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var stocks = await _context.Stocks!.ToListAsync();
+            return new Result<IEnumerable<Stock>>(stocks);
         }
 
         public Task<Result<Stock>> UpdateAsync(Stock stock)
