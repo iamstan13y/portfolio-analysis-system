a63c99e663092bbd63066be45401d2b242f1f863
iamstan13y <iamstan13y@gmail.com>
2022-04-25 00:25:54 +0200

    Calculate allocation I.

--- Sources/Analysis.API/Services/AllocationService.cs
+++ Sources/Analysis.API/Services/AllocationService.cs
@@ -1,4 +1,5 @@
-﻿using Analysis.API.Models.Data;
+﻿using Analysis.API.Enums;
+using Analysis.API.Models.Data;
 using Analysis.API.Models.Local;
 using Analysis.API.Models.Repository;
 using ModelLibrary;
@@ -8,10 +9,12 @@ namespace Analysis.API.Services
     public class AllocationService : IAllocationService
     {
         private readonly IStockRepository _stockRepository;
+        private readonly Random _random;
 
         public AllocationService(IStockRepository stockRepository)
         {
             _stockRepository = stockRepository;
+            _random = new Random();
         }
 
         public async Task<Result<Allocation>> CalculateAllocationAsync(AllocationRequest request)
@@ -24,14 +27,39 @@ namespace Analysis.API.Services
                 ProfileType = request.ProfileType
             };
 
-            var stocks = (await _stockRepository.GetAllAsync()).Data;
+            var stocks = (await _stockRepository.GetAllAsync()).Data!.ToList();
 
             for (int i = 0; i < allocation.Period; i++)
             {
-                //           var 
+                stocks.ForEach(stock => CalculateYearReturn(stock, allocation));
             }
 
-            return null;
+            return new Result<Allocation>(allocation);
+        }
+
+        public Return CalculateYearReturn(Stock stock, Allocation allocation)
+        {
+            var returns = new Return();
+            var shares = _random.Next((int)(allocation.StartingAmount / stock.MaxPrice), (int)(allocation.StartingAmount / stock.MinPrice));
+            var stockAllocation = (stock.PercentAllocation / 100) * shares;
+
+            switch (allocation.ProfileType)
+            {
+                case ProfileType.Conservative:
+                    if (stockAllocation > (allocation.StartingAmount / stock.UnitPrice))
+                    {
+                        returns.CumulatedReturn = allocation.Returns!.ToList()!.LastOrDefault()!.CumulatedReturn + stockAllocation;
+                    }
+                    else
+                    {
+                        returns.CumulatedReturn = allocation.Returns!.ToList()!.LastOrDefault()!.CumulatedReturn - stockAllocation;
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return returns;
         }
     }
 }
