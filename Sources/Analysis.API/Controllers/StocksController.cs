d87517749edb549e7c2f7b5926b8a075697d0e13
iamstan13y <iamstan13y@gmail.com>
2022-05-22 11:29:52 +0200

    HTTP Post Stock refactors.

--- Sources/Analysis.API/Controllers/StocksController.cs
+++ Sources/Analysis.API/Controllers/StocksController.cs
@@ -1,7 +1,6 @@
 ﻿using Analysis.API.Models.Data;
 using Analysis.API.Models.Local;
 using Analysis.API.Models.Repository;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Analysis.API.Controllers
@@ -12,10 +11,7 @@ namespace Analysis.API.Controllers
     {
         private readonly IStockRepository _stockRepository;
 
-        public StocksController(IStockRepository stockRepository)
-        {
-            _stockRepository = stockRepository;
-        }
+        public StocksController(IStockRepository stockRepository) => _stockRepository = stockRepository;
 
         [HttpGet]
         public async Task<IActionResult> Get() => Ok(await _stockRepository.GetAllAsync());
@@ -26,9 +22,12 @@ namespace Analysis.API.Controllers
             var result = await _stockRepository.AddAsync(new Stock
             {
                 CompanyName = request.CompanyName,
+                CompanyCode = request.CompanyCode,
                 UnitPrice = request.UnitPrice,
                 MinPrice = request.MinPrice,
-                MaxPrice = request.MaxPrice
+                MaxPrice = request.MaxPrice,
+                Category = request.Category,
+                PercentageRisk = request.PercentageRisk
             });
 
             if (!result.Success) return BadRequest(result);
--- Sources/Analysis.API/Models/Local/StockRequest.cs
+++ Sources/Analysis.API/Models/Local/StockRequest.cs
@@ -1,10 +1,15 @@
-﻿namespace Analysis.API.Models.Local
+﻿using Analysis.API.Enums;
+
+namespace Analysis.API.Models.Local
 {
     public class StockRequest
     {
         public string? CompanyName { get; set; }
+        public string? CompanyCode { get; set; }
         public double UnitPrice { get; set; }
         public double MinPrice { get; set; }
         public double MaxPrice { get; set; }
+        public StockCategory Category { get; set; }
+        public double PercentageRisk { get; set; }
     }
 }
